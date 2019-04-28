cebdf733aaeaf2d63700f5dde2babc704b5b9420
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-28 18:59:03 +0200

    Rename INDEX to indexFilePath in server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 
 const port = process.env.PORT || 3000;
 
-const INDEX = path.join(process.cwd(), "index.html");
+const indexFilePath = path.join(process.cwd(), "index.html");
 
 express()
-  .use((req, res) => res.sendFile(INDEX))
+  .use((req, res) => res.sendFile(indexFilePath))
   .listen(port);
