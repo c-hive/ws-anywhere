18d5dcad02b451d28a7bc492c83d2e77fcb72798
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-04-28 22:21:35 +0200

    Swap process.cwd() with __dirname in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const port = process.env.PORT || 3000;
 
-const indexFilePath = path.join(process.cwd(), "index.html");
+const indexFilePath = path.join(__dirname, "index.html");
 
 express()
   .use((req, res) => res.sendFile(indexFilePath))
