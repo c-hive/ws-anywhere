2737d7d9dc746c94f0097f936f815cd735820535
gomorizsolt <gomori.zsolt1995@gmail.com>
2019-05-15 09:29:15 +0200

    Extend runtime-variables with db uri, install mongoose

--- configs/runtime-variables.js
+++ configs/runtime-variables.js
@@ -2,5 +2,6 @@ const path = require("path");
 
 module.exports = {
   port: process.env.PORT || 3000,
+  dbURI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017",
   indexFilePath: path.join(process.cwd(), "index.html")
 };
